3c63250140ab6e0bc7d6efe2bd99fe49b5d20aa7
radhikacersei <radhika.halde@quantiphi.com>
2023-03-30 18:51:04 +0530

    adding docai creation script

---
+++ code/docai_creation_script/config.py
@@ -0,0 +1,49 @@
+import pathlib
+
+DEST_WORKBENCH_PROCESSOR_NAME = 'auto_processor2'
+GCS_PATH_FOR_LABELLED_DATA_TEST = 'gs://hcwarehouse-pdf-storage/exported-cde-tagged-data/test/'
+GCS_PATH_FOR_LABELLED_DATA_TRAIN = 'gs://hcwarehouse-pdf-storage/exported-cde-tagged-data/train/'
+PROJECT_ID = "q-gcp-10940-hcwarehouse-23-02"
+API_LOCATION = "us"  # Choose "us" or "eu"
+project_number = '739989291402'
+location = 'us'
+
+
+schema_json = """{
+    "displayName": "all-entity-document-schema",
+    "description": "Document schema for purchase order processor.",
+    "entityTypes": [{
+        "name": "custom_extraction_document_type",
+        "baseTypes": ["document"],
+        "properties": [{
+            "name": "barcode_number",
+            "valueType": "string",
+            "occurrenceType": 1
+        }, 
+        {
+            "name": "classification_code",
+            "valueType": "string",
+            "occurrenceType": 1
+        },
+        {
+            "name": "classification_level",
+            "valueType": "string",
+            "occurrenceType": 1
+        },
+        {
+            "name": "file_number",
+            "valueType": "string",
+            "occurrenceType": 1
+        },
+        {
+            "name": "org_code",
+            "valueType": "string",
+            "occurrenceType": 1
+        },
+        {
+            "name": "volume",
+            "valueType": "string",
+            "occurrenceType": 1
+        }]
+    }]
+}"""
