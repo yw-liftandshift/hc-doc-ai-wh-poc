5cbc71dd3517ce0e5ff34254ba27b1f9a106b605
Sergei Stepantsov <sergei.stepantsov@yoppworks.com>
2023-10-20 10:28:03 -0600

    Added processors names output for consistency

--- infra/deployment/terraform/bootstrap/modules/doc_ai_processors/outputs.tf
+++ infra/deployment/terraform/bootstrap/modules/doc_ai_processors/outputs.tf
@@ -14,10 +14,14 @@ output "cde_classifier_type_processor_name" {
   value = google_document_ai_processor.cde_classifier_type.name
 }
 
-output "cde_processor_location" {
-  value = google_document_ai_processor.cde_lrs_type.location
-}
-
 output "cde_lrs_type_processor_display_name" {
   value = google_document_ai_processor.cde_lrs_type.display_name
+}
+
+output "cde_general_type_processor_display_name" {
+  value = google_document_ai_processor.cde_general_type.display_name
+}
+
+output "cde_classifier_type_processor_display_name" {
+  value = google_document_ai_processor.cde_classifier_type.display_name
 }
