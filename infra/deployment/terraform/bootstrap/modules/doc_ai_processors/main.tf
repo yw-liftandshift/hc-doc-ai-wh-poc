76dfc3a433f116fc85f1e24ea7f322e0ad20af6a
Sergei Stepantsov <sergei.stepantsov@yoppworks.com>
2023-10-20 10:14:11 -0600

    Allign all outputs and variables, removed not used ones

--- infra/deployment/terraform/bootstrap/modules/doc_ai_processors/main.tf
+++ infra/deployment/terraform/bootstrap/modules/doc_ai_processors/main.tf
@@ -33,3 +33,18 @@ resource "google_document_ai_processor" "cde_classifier_type" {
   kms_key_name = var.doc_ai_kms_crypto_key
 }
 
+resource "google_storage_bucket" "cde_processor_training" {
+  name     = "${data.google_project.project.project_id}-cde-processor-training-bucket"
+  location = var.doc_ai_kms_keyring_location
+  project  = data.google_project.project.project_id
+
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  encryption {
+    default_kms_key_name = var.doc_ai_kms_crypto_key
+  }
+}
--- infra/deployment/terraform/bootstrap/outputs.tf
+++ infra/deployment/terraform/bootstrap/outputs.tf
@@ -2,12 +2,16 @@ output "doc_ai_warehouse_provisioning_link" {
   value = "https://console.cloud.google.com/ai/docai-warehouse?project=${var.project_id}"
 }
 
-output "cde_lrs_type_processor_id" {
-  value = module.doc_ai_processors.cde_lrs_type_processor_id
-}
-
 output "cde_lrs_type_processor_name" {
   value = module.doc_ai_processors.cde_lrs_type_processor_name
+}
+
+output "cde_general_type_processor_name" {
+  value = module.doc_ai_processors.cde_general_type_processor_name
+}
+
+output "cde_classifier_type_processor_name" {
+  value = module.doc_ai_processors.cde_classifier_type_processor_name
 }
 
 output "cde_lrs_type_processor_display_name" {
