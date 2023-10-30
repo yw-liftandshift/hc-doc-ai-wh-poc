ab0e854bff113a6077d3945a57496a7fee689814
nassery318 <nassery318@gmail.com>
2023-10-30 15:46:09 -0400

    storedate in a normalized format

--- hc-cloud-function/postprocessing.py
+++ hc-cloud-function/postprocessing.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+import re
 
 '''
 This file is responsible to perform post-processing on top of API responses.
@@ -29,10 +30,20 @@ def build_dictionary_and_filename_from_entities(entities, blob_name, file_number
 
     key_val_dict = {}
     file_number_confidence_score = 0
+    
     #Post-Process the cde response
     for item in entities.pb:
         schema_key = schema_map[item.type_] if item.type_ in schema_map else item.type_
         key_val_dict[schema_key] = item.mention_text
+        # if date is formated as (July 1973) then change it to (1973-07-0) otherwise do nothing
+        if schema_key == "date":
+            if re.match(r'^\d{4}-\d{2}$', item.normalized_value.text):
+                date = item.normalized_value.text+"-0"
+                key_val_dict["date"] = date
+            else:
+                date = item.normalized_value.text
+                key_val_dict["date"] = date
+
         if schema_key == "file_number":
             file_number_confidence_score = item.confidence
        
