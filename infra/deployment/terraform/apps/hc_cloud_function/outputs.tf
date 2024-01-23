91028b9c129bb4c1261e998d957239d0cad9a489
Maksym Veliaminov <maksym.veliaminov@yoppworks.com>
2024-01-23 12:42:28 -0500

    Add missing vars

--- infra/deployment/terraform/apps/hc_cloud_function/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "input_pdf_bucket" {
-  value = google_storage_bucket.input_pdf.name
-}
--- infra/deployment/terraform/apps/monitoring/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "alerting_emails" {
-  type        = list(string)
-  description = "Email addresses that will receive monitoring alerts."
-}
