5094ab91b5497bbf1f6f3458f617f52b14b76755
Sergei Stepantsov <sergei.stepantsov@yoppworks.com>
2024-02-02 14:26:05 -0700

    Added bucket and view

--- infra/deployment/terraform/apps/modules/hc_cloud_function/logging.tf
+++ infra/deployment/terraform/apps/modules/hc_cloud_function/logging.tf
@@ -3,12 +3,25 @@ locals {
   filter = "projects/${data.google_project.project.project_id}/logs/${local.logs_buccket_id}"
 }
 
-resource "google_logging_project_sink" "recognition_result_logs" {
-  name = "recognition_result_logs"
+resource "google_logging_project_sink" "recognition_result_logs_bucket" {
+  name = "${local.logs_buccket_id}_sink"
 
   destination = "logging.googleapis.com/projects/${data.google_project.project.project_id}/locations/global/buckets/${local.logs_buccket_id}"
 
   filter = local.filter
 
-  description = "This log contains information about recognition results"
+  description = "The bucket contains information about recognition results"
+}
+
+resource "google_logging_project_bucket_config" "recognition_result_logs_bucket" {
+    project        = data.google_project.project.project_id
+    location       = ver.region
+    retention_days = 30
+    bucket_id      = local.logs_buccket_id
+}
+
+resource "google_logging_log_view" "logging_log_view" {
+  name        = "${local.logs_buccket_id}_view"
+  bucket      = google_logging_project_bucket_config.recognition_result_logs_bucket.id
+  description = "The view for recognition results"
 }
