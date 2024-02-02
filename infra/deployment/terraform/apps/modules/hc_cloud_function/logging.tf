3cd325ddef62621b50e353ddce4f530334fe4572
Sergei Stepantsov <sergei.stepantsov@yoppworks.com>
2024-02-02 14:34:19 -0700

    remove bucket

--- infra/deployment/terraform/apps/modules/hc_cloud_function/logging.tf
+++ infra/deployment/terraform/apps/modules/hc_cloud_function/logging.tf
@@ -13,16 +13,16 @@ resource "google_logging_project_sink" "recognition_result_logs_bucket" {
   description = "The bucket contains information about recognition results"
 }
 
-resource "google_logging_project_bucket_config" "recognition_result_logs_bucket" {
-    project         = data.google_project.project.project_id
-    location        = var.region
-    retention_days  = 30
-    bucket_id       = local.logs_buccket_id
-    enable_analytics = true
-}
+# resource "google_logging_project_bucket_config" "recognition_result_logs_bucket" {
+#     project         = data.google_project.project.project_id
+#     location        = var.region
+#     retention_days  = 30
+#     bucket_id       = local.logs_buccket_id
+#     enable_analytics = true
+# }
 
-resource "google_logging_log_view" "logging_log_view" {
-  name        = "${local.logs_buccket_id}_view"
-  bucket      = google_logging_project_bucket_config.recognition_result_logs_bucket.id
-  description = "The view for recognition results"
-}
+# resource "google_logging_log_view" "logging_log_view" {
+#   name        = "${local.logs_buccket_id}_view"
+#   bucket      = google_logging_project_bucket_config.recognition_result_logs_bucket.id
+#   description = "The view for recognition results"
+# }
