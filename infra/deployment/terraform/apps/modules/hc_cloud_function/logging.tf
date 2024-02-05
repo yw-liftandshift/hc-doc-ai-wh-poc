ef24d0baa30738714926d7592ab35fdb7f2e45c0
SergeySt <stepancoff@gmail.com>
2024-02-05 11:54:11 -0700

    Dawp 98 create log sink type logging bucket terraform (#35)
    * Add loging permission for HC Cloud Function Service Account custom role
    
    * Added new Sink and Environment variable ID
    
    * Added bucket and view
    
    * Added bucket and view
    
    * enabled analitics
    
    * remove bucket
    
    * remove bucket
    
    * remove bucket
    
    * Enable analytics
    
    * Enable analytics
    
    * Renaming
    
    * Renaming
    
    * Fixed typo
    
    * Fixed typo
    
    * Changed name of log bucket
    
    * Corrected sink destination
    
    ---------
    
    Co-authored-by: Sergei Stepantsov <sergei.stepantsov@yoppworks.com>

---
+++ infra/deployment/terraform/apps/modules/hc_cloud_function/logging.tf
@@ -0,0 +1,28 @@
+locals {
+  log_bucket_id = "recognition_output_log"
+  filter = "projects/${data.google_project.project.project_id}/logs/${local.log_bucket_id}"
+}
+
+resource "google_logging_project_sink" "recognition_result_log_sink" {
+  name = "${local.log_bucket_id}_sink"
+
+  destination = "logging.googleapis.com/projects/${data.google_project.project.project_id}/locations/${var.region}/buckets/${local.log_bucket_id}"
+
+  filter = local.filter
+
+  description = "The bucket contains information about recognition results"
+}
+
+resource "google_logging_project_bucket_config" "recognition_result_log_bucket" {
+    project         = data.google_project.project.project_id
+    location        = var.region
+    retention_days  = 30
+    bucket_id       = local.log_bucket_id
+    enable_analytics = true
+}
+
+resource "google_logging_log_view" "recognition_result_log_view" {
+  name        = "${local.log_bucket_id}_view"
+  bucket      = google_logging_project_bucket_config.recognition_result_log_bucket.id
+  description = "The view for recognition results"
+}
