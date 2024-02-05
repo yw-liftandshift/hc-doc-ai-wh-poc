bbc0cc57b627e72896cc1e49ffceb2492b61478a
Sergei Stepantsov <sergei.stepantsov@yoppworks.com>
2024-02-05 09:13:46 -0700

    Fixed typo

--- infra/deployment/terraform/apps/modules/hc_cloud_function/logging.tf
+++ infra/deployment/terraform/apps/modules/hc_cloud_function/logging.tf
@@ -1,12 +1,12 @@
 locals {
-  log_buccket_id = "recognition_result_log"
-  filter = "projects/${data.google_project.project.project_id}/logs/${local.log_buccket_id}"
+  log_bucket_id = "recognition_result_log"
+  filter = "projects/${data.google_project.project.project_id}/logs/${local.log_bucket_id}"
 }
 
 resource "google_logging_project_sink" "recognition_result_log_sink" {
-  name = "${local.log_buccket_id}_sink"
+  name = "${local.log_bucket_id}_sink"
 
-  destination = "logging.googleapis.com/projects/${data.google_project.project.project_id}/locations/global/buckets/${local.log_buccket_id}"
+  destination = "logging.googleapis.com/projects/${data.google_project.project.project_id}/locations/global/buckets/${local.log_bucket_id}"
 
   filter = local.filter
 
@@ -17,12 +17,12 @@ resource "google_logging_project_bucket_config" "recognition_result_log_bucket" 
     project         = data.google_project.project.project_id
     location        = var.region
     retention_days  = 30
-    bucket_id       = local.log_buccket_id
+    bucket_id       = local.log_bucket_id
     enable_analytics = true
 }
 
 resource "google_logging_log_view" "recognition_result_log_view" {
-  name        = "${local.log_buccket_id}_view"
+  name        = "${local.log_bucket_id}_view"
   bucket      = google_logging_project_bucket_config.recognition_result_log_bucket.id
   description = "The view for recognition results"
 }
