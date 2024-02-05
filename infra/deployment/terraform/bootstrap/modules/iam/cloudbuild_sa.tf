8d87abd9a284b351198d284d11ff3ff854629abf
Sergei Stepantsov <sergei.stepantsov@yoppworks.com>
2024-02-05 15:33:38 -0700

    Added artifactregistry permission to CB SA

--- infra/deployment/terraform/bootstrap/modules/iam/cloudbuild_sa.tf
+++ infra/deployment/terraform/bootstrap/modules/iam/cloudbuild_sa.tf
@@ -2,6 +2,7 @@ locals {
   cloudbuild_sa_email = "${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
 
   cloudbuild_sa_roles = [
+    "roles/artifactregistry.admin",
     "roles/compute.admin",
     "roles/compute.networkAdmin",
     "roles/datastore.owner",
